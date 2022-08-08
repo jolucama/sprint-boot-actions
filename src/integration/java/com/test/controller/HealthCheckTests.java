0e23c577353bd1115da693e01b584bbcf12f4b2c
StephLabuschagne <stephanus.labuschagne@infinityworks.com>
2022-08-08 13:08:59 +0100

    1234 Add integration tests

---
+++ src/integration/java/com/test/controller/HealthCheckTests.java
@@ -0,0 +1,45 @@
+package com.test.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.web.server.LocalManagementPort;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class HealthCheckTests {
+
+  @LocalManagementPort
+  private int managementPort;
+
+  @Autowired
+  private ManagementServerProperties managementServerProperties;
+
+  @Test
+  public void healthCheck() {
+    assertThat(
+        new RestTemplate()
+            .getForEntity(
+                UriComponentsBuilder.fromHttpUrl(getManagementPath() + "/actuator/health")
+                    .build()
+                    .toUri(),
+                String.class)
+            .getStatusCode())
+        .isEqualTo(HttpStatus.OK);
+  }
+
+  private String getManagementPath() {
+    final String managementPath = managementServerProperties.getServlet().getContextPath();
+
+    return "http://localhost:" + managementPort + managementPath;
+  }
+}
